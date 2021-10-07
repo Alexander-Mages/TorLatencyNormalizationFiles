8216a0b7a7e644446819ca8cdbeea0952f0c1c22
Alex Mages <alex@localhost.localdomain>
2021-10-07 10:38:11 -0500

    mannwhitneyu

--- OriginalFiles/ks-or-t-test.py
+++ OriginalFiles/ks-or-t-test.py
@@ -1,15 +1,16 @@
 from __future__ import print_function
 import numpy as np
-from scipy.stats import ttest_ind, ttest_ind_from_stats, ks_2samp
+from scipy.stats import ttest_ind, ttest_ind_from_stats, ks_2samp, mannwhitneyu
 from scipy.special import stdtr
 import sys
+import time
 import csv
 arg1 = sys.argv[1]
 arg2 = sys.argv[2]
 ksorttest = sys.argv[3]
 
 if not arg1 or not arg2 or not ksorttest:
-    print("usage: t-test.py sample1.csv sample2.csv test-type(t-test/ks-test)")
+    print("usage: t-test.py sample1.csv sample2.csv test-type(t-test/ks-test/mannwhitneyu)")
 
 #by default deals with line or space seperated numbers
 a = np.genfromtxt(sys.argv[1])
@@ -21,6 +22,9 @@ if ksorttest == "t-test":
 elif ksorttest == "ks-test":
     #kstest
     x = ks_2samp(a, b)
+    print(x)
+elif ksorttest == "mannwhitneyu":
+    x = mannwhitneyu(a, b)
     print(x)
 else:
     print("invalid test type\nusage: t-test.py sample1.csv sample2.csv test-type(t-test/ks-test)")
