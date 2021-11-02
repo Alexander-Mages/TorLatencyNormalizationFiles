cf7894ccc09e36afae49b7209fcd4ee9242b8f10
Alexander Mages <magesalexander123@gmail.com>
2021-11-02 18:49:26 -0500

    enabled predicted circuits, stream attachment not functioning yet

--- OriginalFiles/customtorcircuit.py
+++ OriginalFiles/customtorcircuit.py
@@ -5,6 +5,7 @@ import subprocess
 import argparse
 from termcolor import colored
 import time
+import requests
 
 #makes a new circuit with custom path and stops tor from creating new ones
 #note: if you add more than 3 fingerprints, the circuit just extends to whatever size you want
@@ -12,7 +13,7 @@ import time
 #script takes one argument: the path selection.
 #should be in this format: '','','' -vv. e.g. '00240ECB2B535AA4C1E1874D744DFA6AF2E5E941','00283B5564E3072DCDDAB31D6EF622DD49BF524F','0011BD2485AD45D984EC4159C88FC066E5E3300E' -vv
 if len(sys.argv[0]) < 1:
-    print("ERR: No Argument\nusage: script.py -v [2-3 (one is default)] 'guard_fingerprint','middleman_fingerprint','exit_fingerprint'")
+    print("ERR: No Argument\nusage: script.py -v [2-3 (one is default)] 'guard','middleman','exit'")
     sys.exit(1)
 
 parser = argparse.ArgumentParser()
@@ -33,7 +34,12 @@ else:
 #for future stuff
 num_circs = 3
 
-def printsum(line):
+def streamAnomaly():
+    alert = "ANOMALY: Unexpected Stream Event Detected. Investigate Further\nTime Of Event: " + time.time()
+    print(colored(alert, 'red'))
+    #log alert to file at some point along with the other output
+
+def UneccecarilyVerboseAndRedundantPrintFunctionSinceUsingPythonsNormalPrintFunctionSomehowBreaksStemsMsgHandler(line):
     print(line)
 
 def startTor(loglevel):
@@ -46,17 +52,18 @@ def startTor(loglevel):
             ],
             'MaxOnionsPending': '0',
             '__DisablePredictedCircuits': '1',
-            'HashedControlPassword': '16:1651BF63EE73164460ED67E7E4046DDB1FE7E408563A9CA566A0D3D538',
+            '__LeaveStreamsUnattached': '1',
+            #'HashedControlPassword': '16:1651BF63EE73164460ED67E7E4046DDB1FE7E408563A9CA566A0D3D538',
             'newcircuitperiod': '999999',
             'maxcircuitdirtiness': '999999',
             #config params for navigaTor
-            'SocksListenAddress': '127.0.0.1:9050',
-            'WarnUnsafeSocks': '0',
-            'CircuitBuildTimeout': '120',
-            'LearnCircuitBuildTimeout': '0',
+            #'SocksListenAddress': '127.0.0.1:9050',
+            #'WarnUnsafeSocks': '0',
+            #'CircuitBuildTimeout': '120',
+            #'LearnCircuitBuildTimeout': '0',
             #'UseMicrodescriptors': '0',
-            'SafeLogging': '0'
-        }, completion_percent=0, take_ownership=True, close_output=False, init_msg_handler=printsum
+            #'SafeLogging': '0'
+        }, completion_percent=0, take_ownership=True, close_output=False, init_msg_handler=UneccecarilyVerboseAndRedundantPrintFunctionSinceUsingPythonsNormalPrintFunctionSomehowBreaksStemsMsgHandler
     )
     return tor_process
 
@@ -88,14 +95,38 @@ def ConnectControlPort():
     print("Tor version", controller.get_version())
     return controller
 
+
 tor_process = startTor(log_level)
 controller = ConnectControlPort()
-
+time.sleep(10)
 #creating custom circuit
 print("status: ", controller.get_info('circuit-status'))
-controller.extend_circuit('0',selectedPath)
-print("Circuit extended along custom path ",selectedPath)
+customcircid = controller.new_circuit(selectedPath,'general')
+print("Circuit" + customcircid + "created on custom path ",selectedPath)
 print(colored("established status: " + controller.get_info('circuit-status'), 'green'))
+#controller.attach_stream('1', customcircid)
+print("Attached stream 1 to custom circuit " + customcircid)
+
+def attachStream(stream):
+    if stream.status == 'NEW':
+        print("98")
+        controller.attach_stream(stream.id, customcircid)
+        print("test")
+controller.add_event_listener(attachStream, control.EventType.STREAM)
+print("1")
+#here is the actual experiment stuff,
+#two ideas pop up: launch firefox here and it will give it a stream,
+#or conduct the experiment right here using pycurl or something of that sort, currently I am going to do the latter
+#visits the webredirector.c server on localhost
+requests.get('http://127.0.0.1:4444',
+             proxies={'http': "socks5://127.0.0.1:9050"})
+print("2")
+controller.remove_event_listener(attachStream)
+print("3")
+#watches to ensure no other streams are attached
+#I am not sure how to get the details of the event, but if logging is at DEBUG verbosity, it will include it
+controller.add_event_listener(streamAnomaly, control.EventType.STREAM)
+
 
 
 #blocking stdout watcher
