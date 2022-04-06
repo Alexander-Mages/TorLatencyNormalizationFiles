a28a94bb9e15cd54c4a180c98fb7af31e61407cf
Alex Mages <alex@localhost.localdomain>
2022-04-05 19:04:27 -0500

    naming improvements

--- ExperimentScripts/vivaldiHaskell/VivaldiAttempt2.hs
+++ ExperimentScripts/vivaldiHaskell/VivaldiAttempt2.hs
@@ -1,7 +1,7 @@
 --type Vivaldi definition, returns type Vivaldi
 --constructors: Coordinates creates custom point (origin), Update takes parameters RTT, and LocalCoordinates + RemoteCoordinates of type Vivaldi
 --comment ^ irrelavent as of commit 7b3e2435b382c9a38624b58ee8dd0716f1658de7, Apr 5
-
+--make integers Float
 data Vivaldi = Coordinates Int Int Int
             
 --a is every type, i.e., avoids specifying type
@@ -10,7 +10,7 @@ Update :: (Vivaldi a) => (Int a, Coordinates a, Coordinates a) -> Vivaldi
 --(local - remote) needs to be fixed. can use vector pattern matching as shown in this article: learnyouahaskell.com/syntax-in-functions, 
 --math derived from figure 2 in vivaldi paper: "the simple vivaldi algorithm with a constant timestep"
 --tuning parameter?
-Update rtt local remote = local + (a * (local - remote))
+Update rtt local remote = local + (u * (local - remote))
    
 --specifies that map should be of type Vivaldi
 origin :: Vivaldi
@@ -18,4 +18,5 @@ origin :: Vivaldi
 let origin = Coordinates 0 0 0
 let newPoint = Coordinates 1 1 1
 --100 is RTT
-firstPoint <- Update 100 origin newPoint
+MapV2 <- Update 100 origin newPoint
+
