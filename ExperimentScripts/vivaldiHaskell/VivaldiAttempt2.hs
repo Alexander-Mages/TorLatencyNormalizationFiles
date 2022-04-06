fe9fe3dd567e28fa2d67f040e43afdcbe6b481db
Alexander Mages <magesalexander123@gmail.com>
2022-04-06 12:19:55 -0500

    "map" object list implemented, variable declaration syntax fixes

--- ExperimentScripts/vivaldiHaskell/VivaldiAttempt2.hs
+++ ExperimentScripts/vivaldiHaskell/VivaldiAttempt2.hs
@@ -6,17 +6,23 @@ data Vivaldi = Coordinates Int Int Int
             
 --a is every type, i.e., avoids specifying type
 --at least I think
-Update :: (Vivaldi a) => (Int a, Coordinates a, Coordinates a) -> Vivaldi
+update :: (Vivaldi a) => (Int a, Coordinates a, Coordinates a) -> Vivaldi
 --(local - remote) needs to be fixed. can use vector pattern matching as shown in this article: learnyouahaskell.com/syntax-in-functions, 
 --math derived from figure 2 in vivaldi paper: "the simple vivaldi algorithm with a constant timestep"
 --tuning parameter?
-Update rtt local remote = local + (u * (local - remote))
+update rtt local remote = local + (u * (local - remote))
    
 --specifies that map should be of type Vivaldi
 origin :: Vivaldi
+newpoint :: Vivaldi
 --gives the map a point (origin). i.e. initializes the coordinate system
 let origin = Coordinates 0 0 0
-let newPoint = Coordinates 1 1 1
+    newpoint = Coordinates 1 1 1
+  --initializes list of coordinates
+    map = [origin, newpoint]
+
+
 --100 is RTT
-map <- Update 100 origin newPoint
+map <- update 100 origin newPoint
+
 
