0ba06457d377caa06b8d299549686262b1f6bf0f
Alexander Mages <magesalexander123@gmail.com>
2022-04-06 17:21:03 -0500

    distance formula implemented, nth element in list indexing implemented

--- ExperimentScripts/vivaldiHaskell/VivaldiAttempt2.hs
+++ ExperimentScripts/vivaldiHaskell/VivaldiAttempt2.hs
@@ -1,6 +1,9 @@
 --make integers Float
 --type declaration, and data constructor
-data Vivaldi = Coordinates Float Float Float
+data Vivaldi = Coordinates {  x :: Float
+                            , y :: Float
+                            , z :: Float
+                            }
             
 --Function declaration: function name, argument list, and output
 update :: (Vivaldi a) => (Float a, Coordinates a, Coordinates a) -> Vivaldi
@@ -10,6 +13,12 @@ update :: (Vivaldi a) => (Float a, Coordinates a, Coordinates a) -> Vivaldi
 --Takes rtt, local, and remote coordinates. Returns updated local coordinates
 update rtt local remote = local + ((rtt - abs(local - remote)) * (1 {- < "unit-length vector"-} * (local - remote)))
 
+--function declaration
+distEquation :: (Float a) => (Coordinates a, Coordinates a) -> Float
+--function definition
+--takes local and remote coordinates, returns coordinate distance between the two
+distEquation local remote = (z local) + (z remote) + sqrt(  (((x local)-(x remote))^2) + (((y local)-(y remote))^2)  )
+
 --Type declaration, specifies objects to be of type Vivaldi
 origin :: Vivaldi
 newpoint :: Vivaldi
@@ -18,8 +27,13 @@ let origin = Coordinates 0 0 0
     newpoint = Coordinates 1 1 1
     --initializes list of coordinates
     map = [origin, newpoint]
-    rtt = Int 100
+    rtt = Float 100
 newlocal <- update rtt origin newPoint
 --replaces first element in "map" list with the new local coordinate
 map & element 0 .~ newlocal
+--"show" map
 putStrLn show(map)
+--calculate and show distance betweeen two nodes
+dist = distEquation (map ^? element 0) (map ^? element 1)
+--don't think I need to use show here, but it's here in this glorified pseudocode
+putStrLn show(dist)
