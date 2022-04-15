b5c0551f19fee0aa352843e1ee106f841c5e6358
Alexander Mages <magesalexander123@gmail.com>
2022-04-15 12:49:05 -0500

    vectors implemented, map vector nesting, vectorlen vectordist adapted to vector type, initialization function

--- ExperimentScripts/vivaldiHaskell/VivaldiV3.hs
+++ ExperimentScripts/vivaldiHaskell/VivaldiV3.hs
@@ -1,7 +1,7 @@
 import Data.Map (Map)
 import qualified Data.Map as Map
 import qualified Data.Vector.Dense as Vector
-import qualified Data.Vector.Dense.Operations
+import qualified Data.Vector.Dense.Operations as Operations
 import System.Random
 
 data Coordinates = Coordinates {
@@ -11,23 +11,35 @@ data Coordinates = Coordinates {
     w :: Float
 }
 
-
-let a = Data.Vector.fromList []
 --vector length
 --does this just find the linear distance?
-vectorLength :: Coordinates -> Float
+vectorLength :: Vector -> Float
 vectorLength v =
     sqrt(
     (x v)^2 + (y v)^2 + (z v)^2 + (w v)^2
     )
 
 --vector distance
-vectorDist :: Coordinates -> Coordinates -> Float
+vectorDist :: Vector -> Vector -> Float
 vectorDist x y =
     --functions not implemented
-    addVector(x, scaleVector(-1,))
+    Operations.plus(x, Operations.scale(-1, y))
+
+initialize :: List -> Vector -> Map
+initialize =
+    --equivilent to vector.list, but with the vector.dense type
+    --let a = Vector.listVector [Coordinates 0 0 0 0, Coordinates 1 1 1 1]
+    Data.Map.fromList [("initialPoint",
+    Vector.listVector [(234, x), (182, y), (321, z), (48, w)])]
+    
+
+
+
+
+
+
 
 
 --work in progress function for initializing random point
 --Random generation is horrid in haskell due to functions' requirement to be "pure"
-newRand = randomIO :: IO into
+--newRand = randomIO :: IO into
