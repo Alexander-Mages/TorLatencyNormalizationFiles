2dfd18c266a3dcb8cd63665f87ce5cd697bfe1f3
Alexander Mages <magesalexander123@gmail.com>
2022-04-16 13:08:03 -0500

    one working implementation among many failed

--- ExperimentScripts/vivaldiHaskell/anotherrandomgenerator.hs
+++ ExperimentScripts/vivaldiHaskell/anotherrandomgenerator.hs
@@ -1,5 +1,15 @@
 import System.Random
-import Control.Monad
+import Control.Monad.State.Lazy
 
-rand :: (Random r) -> (r,r) -> State StdGen r
-rand (min,max) = state (RandomR (min,max))
+randomnum :: Random r => (r,r) -> State StdGen r
+randomnum (min,max) = state (randomR (min,max))
+
+randCoord :: State StdGen (Float, Float)
+randCoord = do
+    x <- random (0, 400)
+    y <- random (0, 400)
+    print (read x, read y)
+
+main = do
+    randCoord
+
