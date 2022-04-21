da2c41817d9a64134dee6775770afe1379eeb7a3
Alexander Mages <magesalexander123@gmail.com>
2022-04-21 17:38:24 -0500

    energy minimization start

--- ExperimentScripts/vivaldiHaskell/Vivaldi.hs
+++ ExperimentScripts/vivaldiHaskell/Vivaldi.hs
@@ -27,15 +27,30 @@ initializeCoordinates :: List -> Vector -> Map
 initializeCoordinates =
     --equivilent to vector.list, but with the vector.dense type
     --let a = Vector.listVector [Coordinates 0 0 0 0, Coordinates 1 1 1 1]
-    return Data.Map.fromList [("initialPoint", Vector.listVector [(randomNum, x), (randomNum, y), (randomNum, z), (randomNum, w)])]
+    Data.Map.fromList [("initialPoint",
+        Vector.listVector [(randomNum, x), (randomNum, y), (randomNum, z), (randomNum, w)])]
     --not sure whether it allows IO numbers to be used
 
-addRandomCoordinate :: Map -> Vector -> Map
-addRandomCoordinate vivaldi =
-    Data.Map.insert "PointOne" (Vector.listVector [(randomNum, x), (randomNum, y), (randomNum, z), (randomNum, w)])
+addRandomCoordinateToMap :: Map -> Vector -> Map
+addRandomCoordinateToMap vivaldi name =
+    Data.Map.insert ("Point" ++ show name) (Vector.listVector [(randomNum, x), (randomNum, y), (randomNum, z), (randomNum, w)]) vivaldi
+    --in the absence of the naming parameter, the insert function doesn't need "vivaldi" argument at end as haskell infers it's placement
+
+addCoordinateAndMinimizeEnergy :: Map -> Vector -> Map
+addCoordinateAndMinimizeEnergy vivaldi name =
+    minimizeEnergy (addRandomCoordinateToMap vivaldi name)
+
+minimizeEnergy :: --idk yet
+minimizeEnergy vivaldi =
+    --this is far from functional, but I'm half implementing it to help my conceptualization
+    --need to add error calculation
+    Operations.add((vivaldi ! "initialPoint") Operations.scale(-1 (vivaldi ! "PointOne")))
+    --the keys should NOT remain hardcoded
+    --no idea what pattern arguments are expected in by operations
 
 main :: IO ()
 main = do
     vivaldi <- initializeCoordinates
-
-
+    vivaldi' <- addCoordinateAndMinimizeEnergy vivaldi "one"
+    --this is too imperative like, I should be able to do this in one function call via recursion, where i would be able to specify a number of random coordinates to be added
+    
