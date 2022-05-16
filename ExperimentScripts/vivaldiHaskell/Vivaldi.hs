101a23ae70276e85e4cd905ee58ccaa84eee585b
Alexander Mages <magesalexander123@gmail.com>
2022-05-16 17:09:42 -0500

    innumerable syntax error fixes

--- ExperimentScripts/vivaldiHaskell/Vivaldi.hs
+++ ExperimentScripts/vivaldiHaskell/Vivaldi.hs
@@ -1,62 +1,63 @@
 module Vivaldi where
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
+import Data.Vector (Vector)
 import qualified Data.Vector as Vector
 import System.Random
+import qualified Data.List
 
 --Vector operations
 --elementwise vector addition
-addvec :: Vector -> Vector -> Vector
+addvec :: Vector Double -> Vector Double -> Vector Double
 addvec a b =
 	-- ^ ^ Vector 1, Vector 2
-	Vector.listVector [((x a)+(x b), x), ((y a)+(y b), y), ((z a)+(z b), z), ((w a)+(w b), w)]
+	Vector.fromList [((a ! 0)+(a ! 0)), ((a ! 1)+(b ! 1)), ((a ! 2)+(b ! 2)), ((a ! 3)+(b ! 3))]   ---this syntax need be applied to rest of code 05/16/22
 						-- ^ ^		 ^
 						--key, vector, key of new coordinate
 --vector scaling
-scalevec :: Vector -> Double -> Vector
+scalevec :: Vector Double -> Double -> Vector Double
 scalevec a b =
 	-- ^ ^ Vector, scale factor
-	Vector.listVector [(b(x a), x), (b(y a), y), (b(z a), z), (b(w a), w)]
+	Vector.fromList [(b(x a), x), (b(y a), y), (b(z a), z), (b(w a), w)]
 					-- ^ ^ ^ ^
 					--scale factor, key, vector, key of new coordinate
 
 
 --vector length
 --does this just find the linear distance?
-vectorLength :: Vector -> Double
+vectorLength :: Vector Double -> Double
 vectorLength v =
 	sqrt (
 	x v ^ 2 + y v ^ 2 + z v ^ 2 + w v ^ 2
 	)
 
 --vector distance
-vectorDist :: Vector -> Vector -> Double
+vectorDist :: Vector Double -> Vector Double -> Double
 vectorDist x y =
 	--functions not implemented
-	vectorLength(Operations.plus(x, Operations.scale(-1, y)))
+	vectorLength(addvec(x, scalevec(y, -1)))
 
 --random number generator (between 1 and 400)
 randomNum :: IO [Double]
 randomNum = do
 	return $ randomRs (1,400) <$> newStdGen
 
-initializeCoordinates :: List -> Vector -> Map
+initializeCoordinates :: Map Integer (Vector Double)
 --returns map of ["host{host#}", (randomly generated 4 way vector)]
 initializeCoordinates =
 	--two maps: one holds hosts, denoted host1 host2 etc... the other holds latencies, denoted latency1-2 latency2-4 etc...
-	Data.Map.fromList (
+	Map.fromList (
 		--"zip" combiles elements of two lists into one list of tuples | zip :: [a] -> [b] -> [(a,b)]
 		zip (
 			[0..25] --integers as keys
 			-- ^ \/ both must be scaled along with the # of latencies created
-			(replicate 25 Vector.listVector [(randomNum, x), (randomNum, y), (randomNum, z), (randomNum, w)])
+			(replicate 25 Vector.fromList [(randomNum, x), (randomNum, y), (randomNum, z), (randomNum, w)])
 			)-- ^ replicate :: Int -> a -> [a], creates list of length of first argument and value of second
 	)
 
-initializeLatencies :: List -> Map
---returns map of ["latency{host#}{dest#}", {randomly generated latency}]
+initializeLatencies :: Map Integer Double
 initializeLatencies =
-	Data.Map.fromList (
+	Map.fromList (
 		zip (
 			--fills every combination of items in 2 [1..9] int lists. Scaling requires simply changing the 9 below to desired host quantity
 			concat $ zipWith (zip . repeat) [1..25] $ tails [1..25] --currently creates some tuples with two identical values
@@ -64,23 +65,21 @@ initializeLatencies =
 		)
 	)
 
-errdist :: Double
+errdist :: [Int] -> Map Integer Double -> Map Integer (Vector Double) -> Double
 errdist latencyid latencies hosts =
 	abs (
 		(latencies ! latencyid) -
 		(vectorDist (hosts ! (latencyid ! 0)) (hosts ! (latencyid ! 1))) ^ 2
 	)
-error :: Map -> Map -> Double
+error :: Map Integer Double -> Map Integer (Vector Double) -> Double
 error latencies hosts =
 	sum (map (errdist (concat $ zipWith (zip . repeat) [1..25] $ tails [1..25])))
 -- ^final error value	^applies the preceding function to all latencies, replacing each item with the result
 
-normalizeMap :: Map -> Map -> Int -> Map
-normalizeMap hosts latencies errTarget =
-	--this is going to look rough until I get it solved conceptually :/
-	--pseudocode is a generous categorization
+normalizeMap :: Map Integer Double -> Map Integer (Vector Double) -> Int -> Map Integer (Vector Double)
+normalizeMap latencies hosts errTarget =
 	until (
-		(((error (latencies hosts)) - 1000) < errTarget)				--first arg
+		(((Vivaldi.error (latencies hosts)) - 1000) < errTarget)				--first arg
 		(map repositionSingleCoordinate)										--second arg
 		(concat $ zipWith (zip . repeat) [1..25] $ tails [1..25])	--third arg
 	)
@@ -90,14 +89,15 @@ normalizeMap hosts latencies errTarget =
 --latencies and hosts Maps are not global variables, depending on how haskell handles functions,
 --I may need to pass them as parameters to the "map" function uses (https://stackoverflow.com/questions/51073535/using-map-with-function-that-has-multiple-arguments)
 
-repositionSingleCoordinate :: Int -> Map
-repositionSingleCoordinate latencyid =
-	Data.Map.insert (latencyid ! 0) (
+
+repositionSingleCoordinate :: [Int] -> Map Integer Double -> Map Integer (Vector Double) -> Map Integer (Vector Double)
+repositionSingleCoordinate latencyid latencies hosts =
+	Map.insert (latencyid ! 0) (
 		addvec (
 			(hosts (latencyid ! 0)), --source
 			scalevec (
 				addvec (
-					Vector.listVector[(randomNum, x), (randomNum, y), (randomNum, z), (randomNum, w)],
+					Vector.fromList[(randomNum, x), (randomNum, y), (randomNum, z), (randomNum, w)],
 					scalevec (
 						(((latencies ! latencyid) - vectorLength(addvec((hosts ! (latencyid ! 0)), scalevec((hosts ! (latencyid ! 1)), -1)))) /
 								(vectorLength(addvec((hosts ! (latencyid ! 0)), scalevec((hosts ! (latencyid ! 1)), -1))))),
@@ -120,7 +120,7 @@ main :: IO ()
 main = do
 	hosts <- initializeCoordinates
 	latencies <- initializeLatencies
-	vivaldi <- normalizeMap hosts latencies errTarget
+	normalizeMap hosts latencies errTarget
 	-- ^ the finished system (i think)		-- ^ arbitrary error cutoff
 
 
@@ -131,7 +131,7 @@ main = do
 --pretending this is not here for now
 addRandomCoordinateToMap :: Map -> Vector -> Map
 addRandomCoordinateToMap vivaldi name =
-	Data.Map.insert ("Point" ++ show name) (Vector.listVector [
+	Data.Map.insert ("Point" ++ show name) (Vector.fromList [
 		(randomNum, x), (randomNum, y), (randomNum, z), (randomNum, w)]) vivaldi
 	--in the absence of the naming parameter, the insert function doesn't need "vivaldi" argument at end as haskell infers it's placement
 
@@ -144,7 +144,7 @@ minimizeEnergy vivaldi name rtt =
 	--this is far from functional, but I'm half implementing it to help my conceptualization
 	--need to add error calculation and it's relavent recursion condition
 	Operations.plus (
-		(Vector.listVector [(0, x), (0, y), (0, z), (0, w)]), --origin
+		(Vector.fromList [(0, x), (0, y), (0, z), (0, w)]), --origin
 		Operations.scale(
 			(rtt - vectorLength(
 		Operations.plus (vivaldi ! "initialPoint") Operations.scale(-1 (vivaldi ! ("Point" ++ show name)))
