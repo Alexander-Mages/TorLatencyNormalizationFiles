653cca7c426645df4b643e3a42047231bb9055b0
Alexander Mages <magesalexander123@gmail.com>
2022-07-22 11:04:34 -0500

    remove cache

--- ExperimentScripts/vivaldiHaskell/copyDirToEnvDir.sh
+++
@@ -1,7 +0,0 @@
-#recklessly installing packages, don't want to dirty the github repo,
-#!/bin/bash
-
-cp ./* ~/haskellcompile/
-
-#install commands are as follows
-	#cabal install --lib --package-env . {packagename}
--- ExperimentScripts/vivaldiHaskell/randomNumberAttempts/RNGv2.hs
+++
@@ -1,9 +0,0 @@
-import System.Random (newStdGen, randomRs, Random, RandomGen)
-
-main :: IO ()
-main = do
-  gen <- newStdGen
-  print $ randomList 4 0 400 gen
-
-randomList :: (Random a, RandomGen g) => Int -> a -> a -> g -> [a]
-randomList len lo hi gen = take len $ randomRs (lo, hi) gen
