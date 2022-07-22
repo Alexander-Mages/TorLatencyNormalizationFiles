b2096d409fabf20a6cf265d391208a04ea37edef
Alexander Mages <magesalexander123@gmail.com>
2022-07-22 11:02:18 -0500

    parsec attempt

--- ExperimentScripts/vivaldiHaskell/parser.hs
+++ ExperimentScripts/vivaldiHaskell/parser.hs
@@ -1,7 +1,21 @@
-import Data.Attoparsec.Ping.Win32
+import Text.ParserCombinators.Parsec
 
+pingFile :: GenParser Char st [[String]]
+pingFile = do
+    result <- many pingLine
+    eof
+    return result
 
+pingLine :: GenParser Char st [String]
+PingLine = do
+    result <- blocks
+    eol
+    return result
+
+blocks :: GenParser Char st [String]
+blocks = do
     
+
 main :: IO ()
 main = do
     file <- readFile "C:/Users/amages/Downloads/Archive/FILENEW1"
