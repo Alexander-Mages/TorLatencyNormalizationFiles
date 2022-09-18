d323094e5a355ef30407f6259e2b3363f65a849d
Alexander Mages <magesalexander123@gmail.com>
2022-09-18 17:54:37 -0500

    phoenix and python-port added

---
+++ ExperimentScripts/phoenixCoordinates/NCSim/NCS_phoenix.py
@@ -0,0 +1,362 @@
+# Generated with SMOP  0.41-beta
+from smop.libsmop import *
+# NCS_phoenix.m
+
+    # a simplified Phoenix NC system without considering node churn, distance variation, etc
+    
+@function
+def phoenix(D=None,dim=None,N=None,K=None,C=None,converge_on=None,new_hosts=None,*args,**kwargs):
+    varargin = phoenix.varargin
+    nargin = phoenix.nargin
+
+    # N: Number of nodes
+# K: Number of Neighbors
+    
+    D_change=copy(D)
+# NCS_phoenix.m:7
+    fpre_newhost=0
+# NCS_phoenix.m:9
+    fpre_flashcrowd=0
+# NCS_phoenix.m:10
+    # Parameters
+    C=10
+# NCS_phoenix.m:13
+    #new_host_scale = 20;
+    if (converge_on == 1):
+        new_host_scale=length(new_hosts)
+# NCS_phoenix.m:16
+    
+    # Per Round -> Per Second ...
+    
+    # evaluate the convergence? #
+    if (converge_on == 0):
+        # no: only run for 30 rounds
+        round_bound=30
+# NCS_phoenix.m:24
+    else:
+        # yes: run more 30 rounds and see the convergence procedure of 10 newly
+    # joinging hosts
+        round_bound=60
+# NCS_phoenix.m:29
+    
+    # 30*64 =
+    closest_num=0
+# NCS_phoenix.m:33
+    result_matrix_seq=[]
+# NCS_phoenix.m:34
+    if (N == 1):
+        #    [out_host, in_host] = NMF(D, dim);
+        out_host=zeros(1,dim)
+# NCS_phoenix.m:38
+        in_host=zeros(dim,1)
+# NCS_phoenix.m:39
+        result_matrix_seq=[]
+# NCS_phoenix.m:40
+        if (converge_on == 1):
+            predicted_matrix=dot(out_host,in_host)
+# NCS_phoenix.m:42
+            for round in arange(1,round_bound).reshape(-1):
+                result_matrix_seq=concat([[result_matrix_seq],[D(1,1)]])
+# NCS_phoenix.m:44
+        return out_host,in_host,fpre_newhost,fpre_flashcrowd
+    
+    if (N < K):
+        # NMF Directly #
+        length(D)
+        out_host,in_host=NMF(D,dim,nargout=2)
+# NCS_phoenix.m:53
+        result_matrix_seq=[]
+# NCS_phoenix.m:54
+        if (converge_on == 1):
+            predicted_matrix=dot(out_host,in_host)
+# NCS_phoenix.m:56
+            for round in arange(1,round_bound).reshape(-1):
+                result_matrix_seq=concat([[result_matrix_seq],[predicted_matrix]])
+# NCS_phoenix.m:58
+        return out_host,in_host,fpre_newhost,fpre_flashcrowd
+    
+    neighbor=zeros(N,K)
+# NCS_phoenix.m:65
+    error_in=zeros(1,N)
+# NCS_phoenix.m:66
+    error_out=zeros(1,N)
+# NCS_phoenix.m:67
+    for i in arange(1,N).reshape(-1):
+        out_host[i,arange()]=rand(1,dim)
+# NCS_phoenix.m:69
+        in_host[arange(),i]=rand(dim,1)
+# NCS_phoenix.m:70
+        tmp=randperm(N)
+# NCS_phoenix.m:73
+        point=1
+# NCS_phoenix.m:75
+        for j in arange(1,K).reshape(-1):
+            neighbor[i,j]=- 1
+# NCS_phoenix.m:77
+        for j in arange(1,K).reshape(-1):
+            # fill in neighbor(i, j) #
+            if (i != tmp(point) and D_change(i,tmp(point)) > 0):
+                neighbor[i,j]=tmp(point)
+# NCS_phoenix.m:82
+                point=point + 1
+# NCS_phoenix.m:83
+            else:
+                while (i == tmp(point) or D_change(i,tmp(point)) <= 0):
+
+                    point=point + 1
+# NCS_phoenix.m:86
+                    if (point > N):
+                        break
+
+                #              printf('#d ', j);
+#              neighbor(i, j) = tmp(point);
+#              point = point+1;
+            if (point > N):
+                break
+    
+    # Out : X in the paper, In : Y in the paprt
+#delta_out = delta;
+#delta_in = delta;
+    
+    #for j=1:15
+    
+    w=zeros(N,K,dim)
+# NCS_phoenix.m:110
+    h=zeros(N,dim,K)
+# NCS_phoenix.m:111
+    D_host2landmark=zeros(N,K)
+# NCS_phoenix.m:112
+    D_host2landmark_out=zeros(N,K)
+# NCS_phoenix.m:113
+    D_host2landmark_in=zeros(N,K)
+# NCS_phoenix.m:114
+    #for j = 1:100
+#for j = 1:(iteration/2)
+    fpre_newhost=[]
+# NCS_phoenix.m:118
+    fpre_flashcrowd=[]
+# NCS_phoenix.m:119
+    for round in arange(1,round_bound).reshape(-1):
+        #    fprintf('22');
+        if (round == round_bound):
+            # Stat #
+            average_node_weight=zeros(1,N)
+# NCS_phoenix.m:125
+            node_weight_count=zeros(1,N)
+# NCS_phoenix.m:126
+        if (converge_on == logical_and(1,round) == floor(round_bound / 2)):
+            #        rand_seq = randperm(N);
+#        new_hosts =  rand_seq(1:new_host_scale);
+        # Remove the NC values of the 10 new hosts
+            for ii in arange(1,new_host_scale).reshape(-1):
+                out_host[new_hosts(ii),arange()]=rand(1,dim)
+# NCS_phoenix.m:134
+                in_host[arange(),new_hosts(ii)]=rand(dim,1)
+# NCS_phoenix.m:135
+            # new
+        #
+        if (round == 1):
+            # First round : Joining #
+            for i in arange(1,N).reshape(-1):
+                tmp=randperm(N)
+# NCS_phoenix.m:145
+                neighbor[i,arange()]=tmp(arange(1,K))
+# NCS_phoenix.m:146
+        for i in arange(1,N).reshape(-1):
+            # K: number of landmarks
+        # w: Kxd, h: dxK. The position vectors of all landmarks
+            #         if (round == 1 & i < K)
+#             continue; # Already has NC
+#         end
+            new_hit=0
+# NCS_phoenix.m:161
+            if (converge_on == logical_and(1,round) == floor(round_bound / 2)):
+                for kk in arange(1,new_host_scale).reshape(-1):
+                    if (new_hosts(kk) == i):
+                        new_hit=1
+# NCS_phoenix.m:166
+                        break
+            if (round == logical_or(1,new_hit) == 1):
+                target_host=neighbor(i,arange())
+# NCS_phoenix.m:173
+                target_host=target_host(find(target_host > 0))
+# NCS_phoenix.m:175
+                target_host=target_host(find(D(i,target_host) >= 0))
+# NCS_phoenix.m:176
+                weight_out_vec=zeros(1,length(target_host))
+# NCS_phoenix.m:179
+                weight_in_vec=zeros(1,length(target_host))
+# NCS_phoenix.m:180
+                temp_w=out_host(target_host,arange())
+# NCS_phoenix.m:182
+                temp_h=in_host(arange(),target_host)
+# NCS_phoenix.m:183
+                temp_D_host2landmark=D_change(i,target_host)
+# NCS_phoenix.m:184
+                temp_D_host2landmark_out=temp_D_host2landmark(arange(1,length(target_host)))
+# NCS_phoenix.m:185
+                temp_D_host2landmark_in=temp_D_host2landmark(arange(1,length(target_host)))
+# NCS_phoenix.m:186
+                for ii in arange(1,length(target_host),1).reshape(-1):
+                    if (D(i,target_host(ii)) < 0):
+                        weight_out_vec[ii]=eps
+# NCS_phoenix.m:190
+                        weight_in_vec[ii]=eps
+# NCS_phoenix.m:191
+                    else:
+                        weight_out_vec[ii]=1
+# NCS_phoenix.m:193
+                        weight_in_vec[ii]=1
+# NCS_phoenix.m:194
+                #             size(temp_h')
+#             size(temp_D_host2landmark_in')
+#             size(sqrt(weight_in_vec)')
+                t=weight_lsqnonneg(temp_h.T,temp_D_host2landmark_in.T,sqrt(weight_in_vec).T)
+# NCS_phoenix.m:200
+                out_host[i,arange()]=t.T
+# NCS_phoenix.m:201
+                in_host[arange(),i]=weight_lsqnonneg(temp_w,temp_D_host2landmark_out.T,sqrt(weight_out_vec).T)
+# NCS_phoenix.m:202
+            # target
+            target_host=neighbor(i,arange())
+# NCS_phoenix.m:207
+            #target_host
+            target_host=target_host(find(target_host > 0))
+# NCS_phoenix.m:210
+            actual_K=length(target_host)
+# NCS_phoenix.m:213
+            temp_w=out_host(target_host,arange())
+# NCS_phoenix.m:215
+            temp_h=in_host(arange(),target_host)
+# NCS_phoenix.m:216
+            temp_D_host2landmark=D_change(i,target_host)
+# NCS_phoenix.m:217
+            score_out_vec=zeros(1,actual_K)
+# NCS_phoenix.m:222
+            score_in_vec=zeros(1,actual_K)
+# NCS_phoenix.m:223
+            score_aver_vec=zeros(1,actual_K)
+# NCS_phoenix.m:224
+            weight_out_vec=zeros(1,actual_K)
+# NCS_phoenix.m:226
+            weight_in_vec=zeros(1,actual_K)
+# NCS_phoenix.m:227
+            for index_nb in arange(1,actual_K).reshape(-1):
+                predict_ii_in=dot(temp_w(index_nb,arange()),in_host(arange(),i))
+# NCS_phoenix.m:232
+                predict_ii_out=dot(out_host(i,arange()),temp_h(arange(),index_nb))
+# NCS_phoenix.m:233
+                s1=abs(predict_ii_out - D_change(i,target_host(index_nb)))
+# NCS_phoenix.m:236
+                s2=abs(predict_ii_in - D_change(i,target_host(index_nb)))
+# NCS_phoenix.m:237
+                score_out_vec[index_nb]=s1
+# NCS_phoenix.m:239
+                score_in_vec[index_nb]=s2
+# NCS_phoenix.m:240
+            out_threshold=median(score_out_vec)
+# NCS_phoenix.m:244
+            in_threshold=median(score_in_vec)
+# NCS_phoenix.m:245
+            for ii in arange(1,actual_K).reshape(-1):
+                if (score_out_vec(ii) < out_threshold):
+                    weight_out_vec[ii]=1
+# NCS_phoenix.m:250
+                else:
+                    if (score_out_vec(ii) < dot(out_threshold,C)):
+                        weight_out_vec[ii]=(out_threshold / score_out_vec(ii)) ** 2
+# NCS_phoenix.m:253
+                    else:
+                        weight_out_vec[ii]=eps
+# NCS_phoenix.m:255
+                if (score_in_vec(ii) < in_threshold):
+                    weight_in_vec[ii]=1
+# NCS_phoenix.m:259
+                else:
+                    if (score_in_vec(ii) < dot(in_threshold,C)):
+                        weight_in_vec[ii]=(in_threshold / score_in_vec(ii)) ** 2
+# NCS_phoenix.m:262
+                        #                    weight_in_vec(ii) = 1 - score_in_vec(ii);
+                    else:
+                        weight_in_vec[ii]=eps
+# NCS_phoenix.m:265
+            #         if (round == round_bound)                
+#             
+#             for ii=1:actual_K
+#                 tmp_seq = target_host(ii);
+#                 #tmp_seq
+#                 average_node_weight(tmp_seq) = average_node_weight(tmp_seq) + weight_in_vec(ii) + weight_out_vec(ii);
+#                 node_weight_count(tmp_seq) = node_weight_count(tmp_seq) + 1;
+#             end
+#         end
+            #         temp_w = temp_w(index_nblist_out, :);
+#         temp_h = temp_h(:, index_nblist_in);
+#         temp_D_host2landmark = temp_D_host2landmark(index_nblist);
+            temp_D_host2landmark_out=temp_D_host2landmark(arange(1,actual_K))
+# NCS_phoenix.m:286
+            temp_D_host2landmark_in=temp_D_host2landmark(arange(1,actual_K))
+# NCS_phoenix.m:287
+            # x = lsqnonneg(C,d) 
+        # returns the vector x that minimizes norm(C*x-d) subject to x >= 0. C and d must be real.
+            #  out_host(i, :) [1*d] * In_NCs[d*K] = D_host2landmark[1*K];        
+        #  => out_host(i, :) * h = D_hosts2landmark[1*K];
+        #  => h' * out_host(i, :)' = D_host2landmark';
+#        size(temp_h)
+#        size(temp_D_host2landmark)
+            #        t = lsqnonneg(temp_h', temp_D_host2landmark_in');
+            t=weight_lsqnonneg(temp_h.T,temp_D_host2landmark_in.T,sqrt(weight_in_vec).T)
+# NCS_phoenix.m:303
+            out_host[i,arange()]=t.T
+# NCS_phoenix.m:304
+            #  => w * in_host(:, i) = D_hosts2landmark'[K*1];
+            #        in_host(:, i) = lsqnonneg(temp_w, temp_D_host2landmark_out');
+            in_host[arange(),i]=weight_lsqnonneg(temp_w,temp_D_host2landmark_out.T,sqrt(weight_out_vec).T)
+# NCS_phoenix.m:310
+            #         w = backup_w;
+#         h = backup_h;
+#         D_host2landmark = backup_D_host2landmark;
+        #     if (converge_on == 1 & round <= floor(round_bound/2))
+#         predicted_matrix = out_host*in_host;
+#         rerr = absolute_error(predicted_matrix, D);        
+#         fpre_flashcrowd = [fpre_flashcrowd; median(rerr)];
+#     end
+        if (converge_on == logical_and(1,round) >= floor(round_bound / 2)):
+            #predicted_matrix = out_host*in_host;
+            predicted_matrix=dot(out_host(new_hosts,arange()),in_host(arange(),arange()))
+# NCS_phoenix.m:326
+            rerr=absolute_error(predicted_matrix,D(new_hosts,arange()))
+# NCS_phoenix.m:327
+            fpre_newhost=concat([[fpre_newhost],[median(rerr)]])
+# NCS_phoenix.m:329
+    
+    # normalize_weight = average_node_weight ./ (node_weight_count+eps)
+# all_err = Detailed_Error_SVD(D, dim);
+# 
+# for i=1:N
+#     fprintf('(#.3f, #.3f) ', all_err(i), normalize_weight(i));
+# end
+    
+    # Error - Weight - Relationship
+#average_node_weight./node_weight_count
+    
+    #re=(abs(out_host * in_host-D)./(D+0.1));
+#mean(re)
+    
+    #figure;
+#plot(mean(re), average_node_weight./node_weight_count)
+    
+    if (converge_on == 1):
+        fprintf('New Host: ')
+        for i in arange(1,floor(round_bound / 2)).reshape(-1):
+            fprintf('%.3f ',fpre_newhost(i))
+        fprintf('\n')
+        #     fprintf('FlashCrowd: ');
+#     for i=1:floor(round_bound/2)
+#         fprintf('#.3f ', fpre_flashcrowd(i));
+#     end
+#     fprintf('\n');
+    
+    # rerr=relative_error(out_host * in_host, D);
+# fprintf('Lambda = #.3f, Relative Error: #.3f\n', lambda, NPRE(rerr));
+    fpre_flashcrowd=[]
+# NCS_phoenix.m:365
---
+++ ExperimentScripts/phoenixCoordinates/NCSim/NCSim_main.py
@@ -0,0 +1,271 @@
+# Generated with SMOP  0.41-beta
+from smop.libsmop import *
+# NCSim_main.m
+
+    
+@function
+def NCSim_main(*args,**kwargs):
+    varargin = NCSim_main.varargin
+    nargin = NCSim_main.nargin
+
+    
+    # Simulator for Decentralized Network Coordinate Algorithms (NCSim)
+    
+    # 
+# Version 1.1.0
+# Updated on Jan. 3, 2016
+# 
+# Copyright (C) <2011-2016> by Yang Chen, Fudan University (chenyang@fudan.edu.cn)
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+    
+    ################# Code #################
+    
+    # raw distance matrix #
+    
+    clear
+    load('data_matrix.mat')
+    # PL: 169 * 169 PlanetLa data set
+# Toread: 355 * 355 PlanetLab data set (collected in Mar.-Apr. 2010, 
+#   used in our ACM ReArch'10 paper 'Taming the Triangle Inequality Violations with Network Coordinate System on Real Internet',
+#   http://code.google.com/p/toread/) 
+# kingmatrix: 1740 * 1740 King data set (http://pdos.csail.mit.edu/p2psim/kingdata/)
+#   used in many Network Coordinate papers
+    
+    # DATA = PL; # PlanetLab data set (small)
+    DATA=copy(Toread)
+# NCSim_main.m:45
+    
+    # DATA = king_matrix; # King data set
+    
+    # parameters of NCSim
+    default_dimension=8
+# NCSim_main.m:50
+    
+    max_round=3
+# NCSim_main.m:51
+    
+    re_cdf_on=1
+# NCSim_main.m:52
+    
+    vivaldi_option=0
+# NCSim_main.m:53
+    # selecting Vivaldi branch, 
+#   0 - Vivaldi (basic), original Vivaldi
+#   1 - Vivaldi (height),  original Vivaldi
+#   2 - Vivaldi (TIV aware), used in "Towards Network Triangle Inequality
+#   Violation Aware Distributed Systems" 
+#   (Proc. of ACM IMC, 2007).
+    ides_option=0
+# NCSim_main.m:60
+    # selecting IDES branch:
+#   0 - IDES (nonnegative), ensuring all predicted distances to be nonnegative, 
+#   used in "Phoenix: A Weight-based Network Coordinate System Using Matrix Factorization" 
+#   (IEEE Transactions on Network and Service Management, 2011, Vol. 8, Issue 4)
+#   1 - IDES (SVD)
+#   2 - IDES(NMF)
+    
+    fprintf('\nNCSim (%d Nodes)\n',length(DATA))
+    if (re_cdf_on == 1):
+        #CDF of relative error (RE)
+        fprintf('\nCDF of relative error (RE): \n\n',length(DATA))
+        total_re_phoenix=[]
+# NCSim_main.m:75
+        total_rank_accuracy_phoenix=[]
+# NCSim_main.m:75
+        total_re_vivaldi=[]
+# NCSim_main.m:76
+        total_rank_accuracy_vivaldi=[]
+# NCSim_main.m:76
+        total_re_dmf=[]
+# NCSim_main.m:77
+        total_rank_accuracy_dmf=[]
+# NCSim_main.m:77
+        total_re_ides=[]
+# NCSim_main.m:78
+        total_rank_accuracy_ides=[]
+# NCSim_main.m:78
+        N=length(DATA)
+# NCSim_main.m:80
+        for round in arange(1,max_round).reshape(-1):
+            # NC: Phoenix
+            out_all,in_all,fpre_newhost,fpre_flashcrowd=NCS_phoenix(DATA,default_dimension,length(DATA),32,5,0,[],nargout=4)
+# NCSim_main.m:84
+            predicted_matrix=dot(out_all,in_all)
+# NCSim_main.m:85
+            real_matrix=copy(DATA)
+# NCSim_main.m:85
+            rerr=relative_error(predicted_matrix,DATA)
+# NCSim_main.m:85
+            output_re=store_re(rerr.T,1,1000)
+# NCSim_main.m:86
+            total_re_phoenix=concat([[total_re_phoenix],[output_re]])
+# NCSim_main.m:86
+            fprintf('Phoenix: %.2f ',NPRE(rerr))
+            phoenix_rank_accuracy=rank_accuracy(predicted_matrix,DATA)
+# NCSim_main.m:88
+            total_rank_accuracy_phoenix=concat([[total_rank_accuracy_phoenix],[phoenix_rank_accuracy]])
+# NCSim_main.m:89
+            # 0- basic Vivaldi, 1- Vivaldi (height), 2-Vivaldi TIV aware
+            predicted_matrix=NCS_vivaldi_all(DATA,default_dimension,length(DATA),32,vivaldi_option)
+# NCSim_main.m:94
+            rerr=relative_error(predicted_matrix,DATA)
+# NCSim_main.m:95
+            output_re=store_re(rerr.T,1,1000)
+# NCSim_main.m:96
+            total_re_vivaldi=concat([[total_re_vivaldi],[output_re]])
+# NCSim_main.m:96
+            fprintf('Vivaldi: %.2f ',NPRE(rerr))
+            vivaldi_rank_accuracy=rank_accuracy(predicted_matrix,DATA)
+# NCSim_main.m:98
+            total_rank_accuracy_vivaldi=concat([[total_rank_accuracy_vivaldi],[vivaldi_rank_accuracy]])
+# NCSim_main.m:99
+            #[out_all, in_all, fperr] = NCS_DMF(DATA, default_dimension, 32, 100, 50, 0);
+            out_all,in_all=NCS_DMFSGD(DATA,32,nargout=2)
+# NCSim_main.m:104
+            predicted_matrix=dot(out_all,in_all)
+# NCSim_main.m:105
+            rerr=relative_error(predicted_matrix,DATA)
+# NCSim_main.m:106
+            output_re=store_re(rerr.T,1,1000)
+# NCSim_main.m:108
+            total_re_dmf=concat([[total_re_dmf],[output_re]])
+# NCSim_main.m:108
+            fprintf('DMFSGD: %.2f ',NPRE(rerr))
+            dmf_rank_accuracy=rank_accuracy(predicted_matrix,DATA)
+# NCSim_main.m:110
+            total_rank_accuracy_dmf=concat([[total_rank_accuracy_dmf],[dmf_rank_accuracy]])
+# NCSim_main.m:111
+            #         
+        # IDES
+            tmp=randperm(N)
+# NCSim_main.m:114
+            landmarks=tmp(arange(1,32))
+# NCSim_main.m:115
+            hosts=tmp(arange(32 + 1,N))
+# NCSim_main.m:116
+            D_landmark=DATA(landmarks,landmarks)
+# NCSim_main.m:117
+            D_host2landmark=DATA(hosts,landmarks)
+# NCSim_main.m:118
+            out_l,in_l,out_h,in_h=NCS_IDES_all(D_landmark,D_host2landmark,default_dimension,ides_option,nargout=4)
+# NCSim_main.m:120
+            predicted_matrix=dot(out_h,in_h)
+# NCSim_main.m:121
+            real_matrix=DATA(hosts,hosts)
+# NCSim_main.m:122
+            rerr=relative_error(predicted_matrix,real_matrix)
+# NCSim_main.m:123
+            npre_ides=NPRE(rerr)
+# NCSim_main.m:124
+            output_re=store_re(rerr.T,1,1000)
+# NCSim_main.m:125
+            total_re_ides=concat([[total_re_ides],[output_re]])
+# NCSim_main.m:125
+            fprintf('IDES: %.2f ',NPRE(rerr))
+            ides_rank_accuracy=rank_accuracy(predicted_matrix,real_matrix)
+# NCSim_main.m:127
+            total_rank_accuracy_ides=concat([[total_rank_accuracy_ides],[ides_rank_accuracy]])
+# NCSim_main.m:128
+            fprintf('\n')
+        figure
+        h1=plot(arange(0,1 - 1 / 1000,1 / 1000),mean(total_re_phoenix),'b--')
+# NCSim_main.m:134
+        set(h1,'LineWidth',2)
+        hold('on')
+        h2=plot(arange(0,1 - 1 / 1000,1 / 1000),mean(total_re_vivaldi),'g:')
+# NCSim_main.m:135
+        set(h2,'LineWidth',2)
+        hold('on')
+        h3=plot(arange(0,1 - 1 / 1000,1 / 1000),mean(total_re_dmf),'k-')
+# NCSim_main.m:136
+        set(h3,'LineWidth',2)
+        hold('on')
+        h4=plot(arange(0,1 - 1 / 1000,1 / 1000),mean(total_re_ides),'r-.')
+# NCSim_main.m:137
+        set(h4,'LineWidth',2)
+        hold('on')
+        h0=plot(arange(0,1),concat([0.9,0.9]),'r:')
+# NCSim_main.m:138
+        hold('on')
+        xlabel('Relative Error','FontSize',16)
+        ylabel('Cumulative Distribution Function','FontSize',16)
+        axis(concat([0,1,0,1]))
+        h5=legend('Phoenix','Vivaldi','DMFSGD','IDES','Location','SouthEast')
+# NCSim_main.m:141
+        set(h5,'FontSize',16)
+        RE_filename='NCSim_RECDF_'
+# NCSim_main.m:142
+        tmp_size=length(DATA)
+# NCSim_main.m:143
+        RE_filename=strcat(RE_filename,num2str(tmp_size))
+# NCSim_main.m:144
+        saveas(gcf,RE_filename,'eps')
+        figure
+        percentage_vec=concat([arange(0.01,0.1,0.01),arange(0.2,1,0.1)])
+# NCSim_main.m:150
+        h1=semilogx(percentage_vec,mean(total_rank_accuracy_phoenix),'b--')
+# NCSim_main.m:151
+        set(h1,'LineWidth',2)
+        hold('on')
+        h2=semilogx(percentage_vec,mean(total_rank_accuracy_vivaldi),'g:')
+# NCSim_main.m:152
+        set(h2,'LineWidth',2)
+        hold('on')
+        h3=semilogx(percentage_vec,mean(total_rank_accuracy_dmf),'k-')
+# NCSim_main.m:153
+        set(h3,'LineWidth',2)
+        hold('on')
+        h4=semilogx(percentage_vec,mean(total_rank_accuracy_ides),'r-.')
+# NCSim_main.m:154
+        set(h4,'LineWidth',2)
+        hold('on')
+        xlabel('Fraction of Shortest Paths to Predict (Log Scale)','FontSize',16)
+        ylabel('Cumulative Distribution Function','FontSize',16)
+        axis(concat([dot(1 / 100,0.9),1,0,1]))
+        h5=legend('Phoenix','Vivaldi','DMFSGD','IDES','Location','SouthEast')
+# NCSim_main.m:157
+        set(h5,'FontSize',16)
+        RE_filename='Ranking_Accuracy_'
+# NCSim_main.m:158
+        tmp_size=length(DATA)
+# NCSim_main.m:159
+        RE_filename=strcat(RE_filename,num2str(tmp_size))
+# NCSim_main.m:160
+        saveas(gcf,RE_filename,'eps')
+        phoenix_fpre=seek_percentage(mean(total_re_phoenix),0.5)
+# NCSim_main.m:164
+        phoenix_npre=seek_percentage(mean(total_re_phoenix),0.9)
+# NCSim_main.m:164
+        vivaldi_fpre=seek_percentage(mean(total_re_vivaldi),0.5)
+# NCSim_main.m:165
+        vivaldi_npre=seek_percentage(mean(total_re_vivaldi),0.9)
+# NCSim_main.m:165
+        dmf_fpre=seek_percentage(mean(total_re_dmf),0.5)
+# NCSim_main.m:166
+        dmf_npre=seek_percentage(mean(total_re_dmf),0.9)
+# NCSim_main.m:166
+        ides_fpre=seek_percentage(mean(total_re_ides),0.5)
+# NCSim_main.m:167
+        ides_npre=seek_percentage(mean(total_re_ides),0.9)
+# NCSim_main.m:167
+        fprintf('\nVivaldi|IDES|DMFSGD|Phoenix')
+        fprintf('\n50th Percentile RE: %.3f %.3f %.3f %.3f\n',vivaldi_fpre,ides_fpre,dmf_fpre,phoenix_fpre)
+        fprintf('90th Percentile RE: %.3f %.3f %.3f %.3f\n',vivaldi_npre,ides_npre,dmf_npre,phoenix_npre)
+    
