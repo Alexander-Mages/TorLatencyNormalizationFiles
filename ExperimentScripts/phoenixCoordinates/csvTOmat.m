8cb0ebe1c8597c7da6447bb927861696725c0016
Alexander Mages <magesalexander123@gmail.com>
2023-01-06 14:38:10 -0600

    remove irrelevant commented-code

--- ExperimentScripts/phoenixCoordinates/csvTOmat.m
+++ ExperimentScripts/phoenixCoordinates/csvTOmat.m
@@ -1,5 +1,3 @@
 ds = tabularTextDatastore('OldVivaldiDataOutput.csv');
 tt = tall(ds);
 write('/mnt/memmap/result',tt);
-%writeall(ds,'OldVivaldiDataset.mat','OutputFormat','mat');
-%save('OldVivaldiDataset.mat','p','q')
