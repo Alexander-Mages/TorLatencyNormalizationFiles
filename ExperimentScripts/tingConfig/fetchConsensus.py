c6168d2e80654f6f17b9e8afd5cbc0a3ebff8d0e
Alexander Paul Mages <magesap@client.tingwithlocalwandz.torlatency.isi.deterlab.net>
2023-01-12 13:49:08 -0800

    output path change

--- ExperimentScripts/tingConfig/fetchConsensus.py
+++ ExperimentScripts/tingConfig/fetchConsensus.py
@@ -4,5 +4,8 @@ from stem.descriptor.remote import DescriptorDownloader
 downloader = DescriptorDownloader()
 consensus = downloader.get_consensus(document_handler = DocumentHandler.DOCUMENT).run()[0]
 
-with open('/home/alex/cachedConsensus', "w") as cachedConsensus:
-	cachedConsensus.write(str(consensus))
+with open('/users/magesap/TorLatencyNormalizationFiles/ExperimentScripts/tingOnDeter/ting/tor/data/w/cached-consensus', "w") as cachedConsensus:
+       cachedConsensus.write(str(consensus))
+
+#with open('/home/alex/cachedConsensus', "w") as cachedConsensus:
+#	cachedConsensus.write(str(consensus))
