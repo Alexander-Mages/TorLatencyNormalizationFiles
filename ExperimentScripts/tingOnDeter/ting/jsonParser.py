5e8abe83fb783ebb5cf34c71efa26abad670a65d
Alex Mages <alex@localhost.localdomain>
2023-02-14 10:53:06 -0600

    parser underway

--- ExperimentScripts/tingOnDeter/ting/jsonParser.py
+++ ExperimentScripts/tingOnDeter/ting/jsonParser.py
@@ -1,11 +1,59 @@
 import json
+import argparse
+import sys
 
-print("syntax: python jsonParser.py (filename)")
+parser = argparse.ArgumentParser()
 
-filename = sys.argv[1]
+parser.add_argument("filename")
+parser.add_argument("desiredData")
+parser.add_argument("monkeySeeOrMonkeyDo")
+parser.add_argument("--outputFile")
+parser.add_argument("--jsonLineNumber")
 
-f = open(filename)
+args = parser.parse_args()
 
-data = json.load(f)
+#if len(args) < 4:
+#^TypeError, parse_args() returns a namespace, but doesn't depopulate sys.argv[]. Using that instead.
+if len(sys.argv) < 3:
+    print("syntax: python jsonParser.py filename (data/rtt) (view/save) --output-file outputFile --jsonLineNumber (1/2/3/etc...)\n"
+          "note: rtt refers to ting's estimated x<->y latency and --jsonLineNumber is the line # of the json object accessed")
+else:
+    filename = args.filename
+    desiredData = args.desiredData
+    seeOrSave = args.monkeySeeOrMonkeyDo
+    outputFile = args.outputFile
+    objInFile = args.objInFile
 
-print(data["trials"][xy])
+
+#read line-by-line to extract multiple json objects
+resultList = []
+with open(filename) as file:
+    for jsonObj in file:
+        resultDict = json.loads(jsonObj) #json.loads() reads from string, json.load() reads from file object
+        resultList.append(resultDict)
+
+if (jsonLineNumber is None) & (len(resultList) > 1):
+    print("file contains multiple JSON objects. Please specify line # of intended JSON object via --jsonLineNumber")
+elif (jsonLineNumber is not None):
+
+else:
+    print("")
+
+
+if seeOrSave == "save":
+    save()
+else:
+    see()
+
+
+def see():
+    for result in resultList:
+        print("\ny: " + result["y"]["ip"] + "\nx: " + result["x"]["ip"])
+        print(result["trials"][0]["xy"]["measurements"])
+
+def save():
+    print("saving xy data from " + filename + ". RTTs from circuit " + result["y"]["ip"] + "<-->" + result["x"]["ip"])
+    print(result["trials"][0]["xy"]["measurements"][199])
+    print(result["trials"][0]["xy"]["measurements"][0])
+    print(outputFile)
+
